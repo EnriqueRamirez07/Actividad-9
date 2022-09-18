507256b3f34605a7709cc6d9dbe982373d389587
EnriqueRamirez07 <113850417+EnriqueRamirez07@users.noreply.github.com>
2022-09-18 11:56:34 -0500

    Add files via upload

---
+++ PROC9_P5Juego-main/sketch.js
@@ -0,0 +1,35 @@
+var a1;
+function setup() {
+  createCanvas(800,600);
+  a1=createSprite(400,200,10,10)
+}
+
+function draw() 
+{
+  background(30);
+if(keyIsDown(RIGHT_ARROW)){
+  a1.position.x = a1.position.x - 5;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+drawSprites();
+
+
+
+
+}
+
+
+
+
